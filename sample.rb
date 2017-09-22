97772eff663eb2aa5e81cf4332471a0cdfb7a466
sean.rattigan <sean.rattigan@gmail.com>
2017-09-22 17:25:22 -0400

    more code

--- sample.rb
+++ sample.rb
@@ -1,5 +1,11 @@
+# Text Play
+
 puts "\t\tHello\n"
 
 puts "Please enter your name: "
 name = gets()
 puts "Hello #{name} you look great!"
+
+puts __FILE__     # print the filename.  Useful?  No idea!
+puts __LINE__     # print the current line number in the source file.  Could be handy for debug?
+puts "Multiplication Value : #{24*60*60}"
