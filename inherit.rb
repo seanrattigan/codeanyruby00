9f3071663d8d5acd2c8771dfa8217524854cd9de
sean.rattigan <sean.rattigan@gmail.com>
2017-09-22 21:15:32 -0400

    inheritance

--- inherit.rb
+++ inherit.rb
@@ -2,7 +2,7 @@
 Ruby Classes to demonstrate inheritance
 =end
 
-class MAmmal
+class Mammal
   @@count = 0               # class var to count num instances
   # constructor in Ruby with defaults set
   def initialize(theName = "Not Set", theWeight = 1.01)
@@ -10,6 +10,12 @@ class MAmmal
     @name = theName
     @weight = theWeight
     @@count += 1
+  end
+  def getName()
+    return @name
+  end
+  def getWeight()
+    return @weight
   end
   def to_s    # toString method
       "Name:#@name, Weight:#@weight"  # string formatting of the object.
@@ -31,8 +37,20 @@ class Lion < Mammal
 end
 
 # Chimera inherits from Lion
-class Cat < Mammal
+class Hawkat < Lion
   def fly
     puts "I believe I can fly"
   end
 end
+
+# main program
+dodger = Cat.new 
+puts dodger.getName()
+puts dodger.getWeight() 
+dodger.speak()
+lion1 = Lion.new 
+puts lion1
+lion1.speak()
+myth = Hawkat.new 
+myth.speak()
+myth.fly()
